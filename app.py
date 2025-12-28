b225d5e2be1bab35f358adf3a595bc82872b7a0f
Dasarath C <robotzeror@gmail.com>
2025-12-28 06:03:22 +0530

    I hope this 2nd commit deploys this project

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
--
+
 
 # Configure CS50 Library to use SQLite database
 db_is_new = not os.path.exists("medsafe.db")
